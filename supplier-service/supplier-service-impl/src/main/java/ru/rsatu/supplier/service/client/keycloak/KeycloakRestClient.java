ec807b6a66124fd9c719a46e9efb48c1db6fd95a
gleb <xypexs173@gmail.com>
2024-01-07 16:36:37 +0300

    Started token issue from keycloak 2

--- supplier-service/supplier-service-impl/src/main/java/ru/rsatu/supplier/service/client/keycloak/KeycloakRestClient.java
+++ supplier-service/supplier-service-impl/src/main/java/ru/rsatu/supplier/service/client/keycloak/KeycloakRestClient.java
@@ -1,8 +1,20 @@
 package ru.rsatu.supplier.service.client.keycloak;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.reactive.RestForm;
+import ru.rsatu.model.keycloak.TokenResponse;
+import ru.rsatu.supplier.data.client.RestClientConfig;
 
-@RegisterRestClient
+import java.util.Map;
+
+@RegisterRestClient(configKey = RestClientConfig.Key.KEYCLOAK_API_KEY)
 public interface KeycloakRestClient {
-    
+    @POST
+    @Path("/realms/TRADE/protocol/openid-connect/token")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    TokenResponse issueToken(@RestForm Map<String, String> filters);
 }
--- supplier-service/supplier-service-impl/src/main/java/ru/rsatu/supplier/service/client/trade/StorageRestClient.java
+++ supplier-service/supplier-service-impl/src/main/java/ru/rsatu/supplier/service/client/trade/StorageRestClient.java
@@ -5,10 +5,13 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.apache.http.HttpStatus;
 import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import ru.rsatu.supplier.data.client.RestClientConfig;
 import ru.rsatu.supplier.dto.storage.StorageResponseDto;
+import ru.rsatu.supplier.utils.RestClientAuthContext;
 
 @Path("/storage")
 @RegisterRestClient(configKey = RestClientConfig.Key.TRADE_SERVICE_API_KEY)
@@ -19,4 +22,12 @@ public interface StorageRestClient {
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     StorageResponseDto getStorageInfo(@PathParam("id") Long storageId);
+
+    @ClientExceptionMapper
+    default void proceedException(Response response) {
+        if (response.getStatus() != HttpStatus.SC_UNAUTHORIZED)
+            return;
+        RestClientAuthContext.resetToken();
+    }
+
 }
